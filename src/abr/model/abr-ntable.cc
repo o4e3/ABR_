bfdac4c96e851d7f4db18a9d29b7d552cff1940e
suin <suin@DESKTOP-7CDRMMS.localdomain>
2026-02-24 11:46:29 +0900

    NT test

--- src/abr/model/abr-ntable.cc
+++ src/abr/model/abr-ntable.cc
@@ -1,5 +1,7 @@
 #include "abr-ntable.h"
 
+#include <limits>
+
 namespace ns3
 {
 namespace abr
@@ -11,13 +13,9 @@ NeighborTableEntry::NeighborTableEntry(Ipv4Address neighbor, uint32_t assocTick)
 {
 }
 
-NeighborTableEntry::~NeighborTableEntry()
-{
-}
+NeighborTableEntry::~NeighborTableEntry() = default;
 
-NeighborTable::NeighborTable()
-{
-}
+NeighborTable::NeighborTable() = default;
 
 bool
 NeighborTable::HasNeighbor(Ipv4Address neighbor)
@@ -28,19 +26,38 @@ NeighborTable::HasNeighbor(Ipv4Address neighbor)
 uint32_t
 NeighborTable::GetAssocTick(Ipv4Address neighbor)
 {
-    // TODO
+    auto it = m_neighborTable.find(neighbor);
+    if (it == m_neighborTable.end())
+    {
+        return 0; // unknown neighbor
+    }
+    return it->second.GetAssocTick();
 }
 
 void
 NeighborTable::InsertTick(Ipv4Address neighbor)
 {
-    // TODO
+    auto it = m_neighborTable.find(neighbor);
+
+    // 처음 본 이웃이면 tick=1로 시작
+    if (it == m_neighborTable.end())
+    {
+        m_neighborTable.emplace(neighbor, NeighborTableEntry(neighbor, 1));
+        return;
+    }
+
+    // 이미 있으면 tick++
+    uint32_t t = it->second.GetAssocTick();
+    if (t != std::numeric_limits<uint32_t>::max())
+    {
+        it->second.SetAssocTick(t + 1);
+    }
 }
 
 bool
-DeleteNeighbor(Ipv4Address neighbor)
+NeighborTable::DeleteNeighbor(Ipv4Address neighbor)
 {
-    // TODO
+    return m_neighborTable.erase(neighbor) > 0;
 }
 
 } // namespace abr
